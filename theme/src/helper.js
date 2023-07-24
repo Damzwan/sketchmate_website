5a155ff3e75bab0c4a43a105009fa6f02a388d54
aku6666 <damian@mygameplan.ai>
2023-07-24 16:02:34 +0200

    seo and legal page

---
+++ theme/src/helper.js
@@ -0,0 +1,8 @@
+export async function sortPosts(posts) {
+    posts.sort((a, b) => {
+        if (isBefore(a.publishDate, b.publishDate)) return 1;
+        if (isBefore(b.publishDate, a.publishDate)) return -1;
+        return 0;
+    });
+    return posts;
+}
