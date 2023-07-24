2c5056c6357b25f766e4e895647376fb5fc22e7e
aku6666 <damian@mygameplan.ai>
2023-07-24 14:25:03 +0200

    various improvements

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -5,4 +5,5 @@ export default {
 	name: `SketchMate`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+	app_url: 'https://app.sketchmate.ninja',
 };
