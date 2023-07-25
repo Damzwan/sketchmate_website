fa0cfca0cad48d53c784e0bb17f119b9d6c9652e
aku6666 <damian@mygameplan.ai>
2023-07-25 15:54:11 +0200

    fixed image url

--- theme/src/pages/rss.xml.js
+++ theme/src/pages/rss.xml.js
@@ -4,13 +4,15 @@ import settings from '../config/settings';
 export function get(context) {
     const postImportResult = import.meta.glob('./blog/posts/*.{md,mdx}', {eager: true});
     const posts = Object.values(postImportResult);
+    const site = context.site.toString()
+    const siteUrl = site.endsWith('/') ? site.slice(0, -1) : site;
     return rss({
         title: settings.title,
         description: settings.description,
         site: context.site,
         items: posts.map((post) => ({
             link: post.url,
-            content: `<img src="${context.site}${post.frontmatter.featuredImage}" alt="${post.frontmatter.title}"/>`,
+            content: `<img src="${siteUrl}${post.frontmatter.featuredImage}" alt="${post.frontmatter.title}"/>`,
             ...post.frontmatter,
         })),
     });
