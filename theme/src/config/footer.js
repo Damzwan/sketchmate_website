2f985ad00f9cb68082ba01075302783bd59dd046
aku6666 <damian@mygameplan.ai>
2023-08-25 12:25:16 +0200

    incorporated feedback from winand

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,56 +1,56 @@
 export const footerSocials = [
-	{
-		name: 'Github',
-		url: 'https://github.com/Damzwan/sketchmate',
-		icon: "mdi:github",
-	},
-	{
-		name: 'Discord',
-		url: 'https://discord.gg/EvDa9E5A6r',
-		icon: "ic:baseline-discord",
-	},
-	{
-		name: 'Linkedin',
-		url: 'https://www.linkedin.com/in/damian-vlaicu-021564194/',
-		icon: "mdi:linkedin",
-	},
-	{
-		name: 'mail',
-		url: 'mailto:news@sketchmate.ninja',
-		icon: "mdi:email",
-	},
+    {
+        name: 'Github',
+        url: 'https://github.com/Damzwan/sketchmate',
+        icon: "mdi:github",
+    },
+    {
+        name: 'Discord',
+        url: 'https://discord.gg/EvDa9E5A6r',
+        icon: "ic:baseline-discord",
+    },
+    {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/damian-vlaicu-021564194/',
+        icon: "mdi:linkedin",
+    },
+    {
+        name: 'mail',
+        url: 'mailto:contact@sketchmate.ninja',
+        icon: "mdi:email",
+    },
 ]
 
 export const footerLists = [
-	{
-		title: 'Home',
-		items: [
-			{
-				title: 'Home',
-				slug: '/',
-			},
-		],
-	},
-	{
-		title: 'Company',
-		items: [
-			{
-				title: 'About',
-				slug: '/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
-				title: 'Contact',
-				slug: '/contact',
-			},
-			{
-				title: 'Legal',
-				slug: '/legal',
-			},
-		],
-	},
+    {
+        title: 'Home',
+        items: [
+            {
+                title: 'Home',
+                slug: '/',
+            },
+            {
+                title: 'Blog',
+                slug: '/blog',
+            },
+        ],
+    },
+    {
+        title: 'Company',
+        items: [
+            {
+                title: 'About',
+                slug: '/about',
+            },
+            {
+                title: 'Contact',
+                slug: '/contact',
+            },
+            {
+                title: 'Legal',
+                slug: '/legal',
+            },
+        ],
+    },
 
 ];
